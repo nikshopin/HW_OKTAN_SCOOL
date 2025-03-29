9b2606c8bdd39fe94182d85c18dda7f5aa105ccf
Shopin <nikshopin@gmail.com>
2025-03-29 16:35:14 +0200

    home work JS 1 completed

---
+++ JS/HW_1/HW1.2/script/init.js
@@ -0,0 +1,11 @@
+let firstName;
+let middleName;
+let lastName;
+firstName = 'Шопин';
+middleName = 'Николай';
+lastName = 'Николаевич';
+
+// let person=firstName+ ' ' + middleName + ' ' + middleName;
+let person = `${firstName} ${middleName} ${middleName}`;
+console.log(person);
+
---
+++ JS/HW_1/HW1.4/script/init.js
@@ -0,0 +1,7 @@
+let firstName = prompt("Введите свое имя: ");
+let middleName = prompt("Введите свое отчество: ");
+let lastName = prompt("Введите свое фамилию : ");
+
+let person = `${lastName} ${middleName} ${middleName}`;
+
+console.log(person);
